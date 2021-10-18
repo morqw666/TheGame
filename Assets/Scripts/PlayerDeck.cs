886ac2e5309b2cd4d25a71c5e5e90c3811eb331c
DESKTOP-9R7S1JD\voo-v <morqw666@gmail.com>
2021-10-18 16:59:42 +0300

    added card leveling up to 2 lvl

--- Assets/Scripts/PlayerDeck.cs
+++ Assets/Scripts/PlayerDeck.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,7 @@ public class PlayerDeck : MonoBehaviour
     [SerializeField] public Text Health;
     private int health = 200;
     private List<Card> PlayerCardDeck = new List<Card>();
+    private Renderer _renderer;
     private void Start()
     {
         Health.text = health.ToString();
@@ -17,21 +19,94 @@ public class PlayerDeck : MonoBehaviour
     {
         for (int i = 0; i < _podiums.Count; i++)
         {
-            var cardOnPodium = _podiums[i].GetCard();
-            if (!_podiums[i].IsEmpty() && cardOnPodium.GetMaterial() == card.GetMaterial() && cardOnPodium.level < 4) //_podiums[i].Contains(card)
+            if (!_podiums[i].IsEmpty())
             {
-                card.level = cardOnPodium.level + 1;
-                _podiums[i].CardLevel.text = card.level.ToString();
-                _podiums[i].Destroy();
-                PlayerCardDeck.Remove(cardOnPodium);
-                PlayerCardDeck.Add(card);
-                _podiums[i].SetCard(card);
-                return true;
+                var cardOnPodium = _podiums[i].GetCard();
+                if (IsPodiumsContainLevelTwo())
+                {
+                    var cardLvlTwo = CardLevelTwo();
+                    if (cardLvlTwo.GetMaterial() == cardOnPodium.GetMaterial() && cardLvlTwo.GetMaterial() == card.GetMaterial() && cardOnPodium.level < 2 && IsEmptyPodium())
+                    {
+                        card.level = 3;
+                        _podiums[i].CardLevel.text = card.level.ToString();
+                        _podiums[i].Destroy();
+                        var podiumCardLevelTwo = WhatPodiumContainLevelTwo(cardLvlTwo);
+                        podiumCardLevelTwo.Destroy();
+                        podiumCardLevelTwo.CardLevel.text = " ";
+                        PlayerCardDeck.Remove(cardLvlTwo);
+                        PlayerCardDeck.Remove(cardOnPodium);
+                        PlayerCardDeck.Add(card);
+                        _podiums[i].SetCard(card);
+                        return true;
+                    }
+                }
+                if (cardOnPodium.GetMaterial() == card.GetMaterial() && cardOnPodium.level < 2 && IsEmptyPodium())
+                {
+                    card.level = 2;
+                    _podiums[i].CardLevel.text = card.level.ToString();
+                    _podiums[i].Destroy();
+                    PlayerCardDeck.Remove(cardOnPodium);
+                    PlayerCardDeck.Add(card);
+                    _podiums[i].SetCard(card);
+                    return true;
+                }
             }
             if (_podiums[i].IsEmpty())
             {
                 PlayerCardDeck.Add(card);
                 _podiums[i].SetCard(card);
+                return true;
+            }
+        }
+        return false;
+    }
+    private bool IsPodiumsContainLevelTwo()
+    {
+        for (int i = 0; i < _podiums.Count; i++)
+        {
+            try
+            {
+                var card = _podiums[i].GetCard();
+                if (card.level == 2)
+                {
+                    return true;
+                }
+            } catch (NullReferenceException) {
+                continue;
+            }
+            
+        }
+        return false;
+    }
+    private Card CardLevelTwo()
+    {
+        for (int i = 0; i < _podiums.Count; i++)
+        {
+            var card = _podiums[i].GetCard();
+            if (card.level == 2)
+            {
+                return card;
+            }
+        }
+        return null;
+    }
+    private Podium WhatPodiumContainLevelTwo(Card cardLvlTwo)
+    {
+        for (int i = 0; i < _podiums.Count; i++)
+        {
+            if (_podiums[i].GetCard() == cardLvlTwo)
+            {
+                return _podiums[i];
+            }
+        }
+        return null;
+    }
+    private bool IsEmptyPodium()
+    {
+        for (int i = 0; i < _podiums.Count; i++)
+        {
+            if (_podiums[i].IsEmpty())
+            {
                 return true;
             }
         }
