c3cc0687df061d8a5e2534beccf2a36b49db62e8
DESKTOP-9R7S1JD\voo-v <morqw666@gmail.com>
2021-10-18 17:22:10 +0300

    added card leveling to lvl 4 (with bug)

--- Assets/Scripts/PlayerDeck.cs
+++ Assets/Scripts/PlayerDeck.cs
@@ -19,13 +19,37 @@ public class PlayerDeck : MonoBehaviour
     {
         for (int i = 0; i < _podiums.Count; i++)
         {
-            if (!_podiums[i].IsEmpty())
+            if (!_podiums[i].IsEmpty() && IsEmptyPodium())
             {
                 var cardOnPodium = _podiums[i].GetCard();
+                if (IsPodiumsContainLevelThree() && IsPodiumsContainLevelTwo())
+                {
+                    var cardLvlTwo = CardLevelTwo();
+                    var cardLvlThree = CardLevelThree();
+                    if (cardLvlTwo.GetMaterial() == cardOnPodium.GetMaterial() && cardLvlTwo.GetMaterial() == card.GetMaterial() && cardLvlTwo.GetMaterial() == cardLvlThree.GetMaterial() && cardOnPodium.level < 2)
+                    {
+                        card.level = 4;
+                        _podiums[i].CardLevel.text = card.level.ToString();
+                        _podiums[i].Destroy();
+                        var podiumCardLevelTwo = WhatPodiumContainLevelTwo(cardLvlTwo);
+                        podiumCardLevelTwo.Destroy();
+                        podiumCardLevelTwo.CardLevel.text = " ";
+                        var podiumCardLevelThree = WhatPodiumContainLevelThree(cardLvlThree);
+                        podiumCardLevelThree.Destroy();
+                        podiumCardLevelThree.CardLevel.text = " ";
+                        PlayerCardDeck.Remove(cardLvlTwo);
+                        PlayerCardDeck.Remove(cardLvlThree);
+                        PlayerCardDeck.Remove(cardOnPodium);
+                        PlayerCardDeck.Add(card);
+                        _podiums[i].SetCard(card);
+                        return true;
+                    }
+                    
+                }
                 if (IsPodiumsContainLevelTwo())
                 {
                     var cardLvlTwo = CardLevelTwo();
-                    if (cardLvlTwo.GetMaterial() == cardOnPodium.GetMaterial() && cardLvlTwo.GetMaterial() == card.GetMaterial() && cardOnPodium.level < 2 && IsEmptyPodium())
+                    if (cardLvlTwo.GetMaterial() == cardOnPodium.GetMaterial() && cardLvlTwo.GetMaterial() == card.GetMaterial() && cardOnPodium.level < 2)
                     {
                         card.level = 3;
                         _podiums[i].CardLevel.text = card.level.ToString();
@@ -40,7 +64,7 @@ public class PlayerDeck : MonoBehaviour
                         return true;
                     }
                 }
-                if (cardOnPodium.GetMaterial() == card.GetMaterial() && cardOnPodium.level < 2 && IsEmptyPodium())
+                if (cardOnPodium.GetMaterial() == card.GetMaterial() && cardOnPodium.level < 2)
                 {
                     card.level = 2;
                     _podiums[i].CardLevel.text = card.level.ToString();
@@ -78,6 +102,26 @@ public class PlayerDeck : MonoBehaviour
         }
         return false;
     }
+    private bool IsPodiumsContainLevelThree()
+    {
+        for (int i = 0; i < _podiums.Count; i++)
+        {
+            try
+            {
+                var card = _podiums[i].GetCard();
+                if (card.level == 3)
+                {
+                    return true;
+                }
+            }
+            catch (NullReferenceException)
+            {
+                continue;
+            }
+
+        }
+        return false;
+    }
     private Card CardLevelTwo()
     {
         for (int i = 0; i < _podiums.Count; i++)
@@ -90,11 +134,34 @@ public class PlayerDeck : MonoBehaviour
         }
         return null;
     }
+    private Card CardLevelThree()
+    {
+        for (int i = 0; i < _podiums.Count; i++)
+        {
+            var card = _podiums[i].GetCard();
+            if (card.level == 3)
+            {
+                return card;
+            }
+        }
+        return null;
+    }
     private Podium WhatPodiumContainLevelTwo(Card cardLvlTwo)
     {
         for (int i = 0; i < _podiums.Count; i++)
         {
             if (_podiums[i].GetCard() == cardLvlTwo)
+            {
+                return _podiums[i];
+            }
+        }
+        return null;
+    }
+    private Podium WhatPodiumContainLevelThree(Card cardLvlThree)
+    {
+        for (int i = 0; i < _podiums.Count; i++)
+        {
+            if (_podiums[i].GetCard() == cardLvlThree)
             {
                 return _podiums[i];
             }
